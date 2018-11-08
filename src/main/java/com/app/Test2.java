0af551f0e48fa7d675159148f6b618eab5d52e87
Madhusmita <infosmita89@gmail.com>
2018-11-08 20:54:26 +0530

    change

--- src/main/java/com/app/Test2.java
+++ src/main/java/com/app/Test2.java
@@ -1,9 +1,11 @@
+package com.app;
+
 public class Test2 {
 	public static void main(String[] args) {
 		int a=2;
 		int b=5;
 		int c=a+b;
-    int d=a+b+c
+         int d=a+b+c;
 		System.out.println(d);
 	}
 
